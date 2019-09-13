35d0c4b727ac5bd53bf3ba307fc891870a62ad3b
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-13 10:24:13 -0400

    Remove the client file

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-//if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+  //app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  })
+  app.use(express.static("client/build"));
+}
+  // app.use('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  // })
 //}
 // Add routes, both API and view
 app.use(routes);
