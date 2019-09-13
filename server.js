3714bd00970110541f8d328a4ebf10fa92455417
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-13 10:25:49 -0400

    Change mongodb setting

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
   //app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.use(express.static("client/build"));
-}
+//}
   // app.use('*', (req, res) => {
   //   res.sendFile(path.join(__dirname + '/client/build/index.html'));
   // })
